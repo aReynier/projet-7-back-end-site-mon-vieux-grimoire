0524f0fb0e536033e16c8b95f90a6249ba5b9f47
Aurore Reynier <reynier.aurore@gmail.com>
2023-08-18 14:20:23 +0200

    green code, helmet, sanitize, dossier .env et sécurisation du signup avec joi

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const bookRoutes = require('./routes/book');
 const userRoutes = require('./routes/user');
+const mongoSanitize = require('express-mongo-sanitize');
 const path = require('path');
+require('dotenv').config();
 
-mongoose.connect('mongodb+srv://default_user:fguynpev@cluster0.iajdy8a.mongodb.net/?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://'+ process.env.MONGO_USER + ':' + process.env.MONGO_PASSWORD + '@cluster0.iajdy8a.mongodb.net/?retryWrites=true&w=majority',
     { useNewUrlParser: true,
       useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie!'))
@@ -13,15 +15,26 @@ mongoose.connect('mongodb+srv://default_user:fguynpev@cluster0.iajdy8a.mongodb.n
 
 const app = express();
 
+var helmet = require('helmet');
+app.use(helmet());
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Cross-Origin-Resource-Policy', 'same-site'); //fix helmet not same origin error
     next();
   });
 
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 
+  app.use(
+    mongoSanitize({
+      replaceWith: '_',
+    }),
+  );
+  
   app.use('/api/books', bookRoutes);
   app.use('/api/auth', userRoutes);
   app.use('/images', express.static(path.join(__dirname, 'images')));
